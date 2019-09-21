ee351e918b9d7bfe6c7aefe046ae6586e0934ce8
agkz1st <ahmed.agk22@gmail.com>
2019-09-21 15:00:45 +0200

    Generate a slug for each node (blog post)

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,20 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const { createFilePath } = require('gatsby-source-filesystem');
 
-// You can delete this file if you're not using it
+exports.onCreateNode = ({ node, actions, getNode }) => {
+	// we used this method to attach the slug to each node.
+	const { createNodeField } = actions;
+
+	if (node.internal.type === 'MarkdownRemark') {
+		const relativeFilePath = createFilePath({
+			node,
+			getNode,
+			trailingSlash: false
+		});
+
+		createNodeField({
+			node,
+			name: 'slug',
+			value: relativeFilePath
+		});
+	}
+};
